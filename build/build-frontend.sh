3ca2257ada68167afc7ad23c3f33465a0898e89b
Pavlo Kerestey <pavlo@kerestey.net>
2023-04-26 21:30:54 +0200

    fix linux zola URL

--- build/build-frontend.sh
+++ build/build-frontend.sh
@@ -12,7 +12,7 @@ then
         cd "$SCRIPTPATH"
         case "$(uname -s)" in
             Linux*)
-                wget -c https://github.com/getzola/zola/releases/download/v0.17.2/  zola-v0.17.2-x86_64-unknown-linux-gnu.tar.gz
+                wget -c https://github.com/getzola/zola/releases/download/v0.17.2/zola-v0.17.2-x86_64-unknown-linux-gnu.tar.gz
                 ;;
             Darwin*)
                 wget -c https://github.com/getzola/zola/releases/download/v0.17.2/zola-v0.17.2-x86_64-apple-darwin.tar.gz
