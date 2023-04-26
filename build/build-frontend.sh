370cf38b683f23bb274b6558a70e4ddc496766f3
Pavlo Kerestey <pavlo@kerestey.net>
2023-04-26 21:25:24 +0200

    introduce automaated frontend download and build

---
+++ build/build-frontend.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -euo pipefail
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+export ZOLACMD=$(command -v zola)
+
+if ! $(test -f "$ZOLACMD")
+then
+    (
+        cd "$SCRIPTPATH"
+        case "$(uname -s)" in
+            Linux*)
+                wget -c https://github.com/getzola/zola/releases/download/v0.17.2/  zola-v0.17.2-x86_64-unknown-linux-gnu.tar.gz
+                ;;
+            Darwin*)
+                wget -c https://github.com/getzola/zola/releases/download/v0.17.2/zola-v0.17.2-x86_64-apple-darwin.tar.gz
+                ;;
+            *)
+                echo "Unsupported OS"
+                exit
+                ;;
+        esac
+        tar xvfz zola-*.tar.gz
+    )
+    ZOLACMD="./build/zola"
+fi
+
+$ZOLACMD build
